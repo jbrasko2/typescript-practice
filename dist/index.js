17505ff560feeb07f5360fa39ce10c3ff05d15ec
jbrasko <jbrasko0508@gmail.com>
2021-09-30 12:23:14 -0400

    add enum and object examples

--- dist/index.js
+++ dist/index.js
@@ -16,8 +16,34 @@ let employee;
 employee = [
     [1, 'Jordan'],
     [2, 'Jack'],
-    [3, 'Jill']
+    [3, 'Jill'],
 ];
 // Union - more than one type
 let pid = 22;
 pid = 'twenty-two';
+// Enum - named constants, numeric or string
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction1.Up);
+console.log(Direction2.Up);
+// Objects
+const user = {
+    id: 1,
+    name: 'Jordan',
+};
+const user1 = {
+    id: 1,
+    name: 'Jordan',
+};
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ age = 30
 let ids: number[] = [1, 2, 3, 4]
 let arr: any[] = [1, true, 'Hello']
 
-
 // Tuple
 let person: [number, string, boolean] = [1, 'Jordan', true]
 // Tuple Array
@@ -20,9 +19,47 @@ let employee: [number, string][]
 employee = [
   [1, 'Jordan'],
   [2, 'Jack'],
-  [3, 'Jill']
+  [3, 'Jill'],
 ]
 
 // Union - more than one type
 let pid: string | number = 22
 pid = 'twenty-two'
+
+// Enum - named constants, numeric or string
+
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right',
+}
+
+console.log(Direction1.Up)
+console.log(Direction2.Up)
+
+// Objects
+const user: {
+  id: number
+  name: string
+} = {
+  id: 1,
+  name: 'Jordan',
+}
+
+type User = {
+  id: number
+  name: string
+}
+
+const user1: User = {
+  id: 1,
+  name: 'Jordan',
+}
