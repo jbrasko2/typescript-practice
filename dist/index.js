10de3dd1bf7c89e7e9bee57aa57c73d1e6ef2cf5
jbrasko <jbrasko0508@gmail.com>
2021-09-30 12:06:36 -0400

    add tuple example

--- dist/index.js
+++ dist/index.js
@@ -6,5 +6,8 @@ let x = 'Hello';
 let age;
 x = true;
 age = 30;
+// Array
 let ids = [1, 2, 3, 4];
 let arr = [1, true, 'Hello'];
+// Tuple
+let person = [1, 'Jordan', true];
--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,10 @@ let age: number
 x = true
 age = 30
 
+// Array
 let ids: number[] = [1, 2, 3, 4]
 let arr: any[] = [1, true, 'Hello']
+
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Jordan', true]
